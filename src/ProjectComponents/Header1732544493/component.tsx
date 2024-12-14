92a1b66bd1ef87e8dc66f3c2d2846ecb3653374e
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-12-14 08:42:55 +0000

    feat: Edit Header1732544493
    Requested change: Remove all elements except the image and change background color to white

--- src/ProjectComponents/Header1732544493/component.tsx
+++ src/ProjectComponents/Header1732544493/component.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-blue-500 text-white p-4 w-full h-full relative">
-      <div className="flex justify-center mb-4">
+    <header className="bg-white p-4 w-full h-full relative">
+      <div className="flex justify-center">
         <img src={`https://raw.githubusercontent.com/56b81caaa87941618cfed6dfb4d34047/Testing_Platform_1732544491/${window.MI_PROJECT_GIT_REF || 'main'}/src/assets/images/02dfdbde981d4596aa48a513a93b1716.jpeg`} alt="Circle" className="rounded-full w-24 h-24" />
       </div>
-      <h1 className="text-2xl font-bold text-center">Welcome</h1>
     </header>
   );
 };
