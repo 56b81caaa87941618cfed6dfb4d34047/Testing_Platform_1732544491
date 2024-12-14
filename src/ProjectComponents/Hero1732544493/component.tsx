54262c23ed7d5d16f3b083243688a9c53be28218
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-12-14 08:38:02 +0000

    feat: Edit Hero1732544493
    Requested change: Remove any gap between the circle image and the top of the component

--- src/ProjectComponents/Hero1732544493/component.tsx
+++ src/ProjectComponents/Hero1732544493/component.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Hero: React.FC = () => {
   
   return (
-    <div className="bg-white py-16 w-full h-full">
-      <div className="flex justify-center mb-8">
-        <img src={`https://raw.githubusercontent.com/56b81caaa87941618cfed6dfb4d34047/Testing_Platform_1732544491/${window.MI_PROJECT_GIT_REF || 'main'}/src/assets/images/02dfdbde981d4596aa48a513a93b1716.jpeg`} alt="Hero" className="w-24 h-24" />
+    <div className="bg-white w-full h-full">
+      <div className="flex justify-center">
+        <img src={`https://raw.githubusercontent.com/56b81caaa87941618cfed6dfb4d34047/Testing_Platform_1732544491/${window.MI_PROJECT_GIT_REF || 'main'}/src/assets/images/02dfdbde981d4596aa48a513a93b1716.jpeg`} alt="Hero" className="w-24 h-24 mt-0" />
       </div>
     </div>
   );
