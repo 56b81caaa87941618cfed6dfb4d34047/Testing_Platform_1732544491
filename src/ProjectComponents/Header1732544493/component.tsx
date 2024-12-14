29d38a0c9f3e4c680d14d5f3c2636a0d0486541f
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-12-14 09:04:12 +0000

    Update source code for component: Header1732544493

--- src/ProjectComponents/Header1732544493/component.tsx
+++ src/ProjectComponents/Header1732544493/component.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+ import React from 'react';
 
-const Header: React.FC = () => {
+const Hero: React.FC = () => {
+  
   return (
-    <header className="bg-white p-4 w-full h-full relative">
+    <div className="bg-white w-full h-full">
       <div className="flex justify-center">
-        <img src={`https://raw.githubusercontent.com/56b81caaa87941618cfed6dfb4d34047/Testing_Platform_1732544491/${window.MI_PROJECT_GIT_REF || 'main'}/src/assets/images/02dfdbde981d4596aa48a513a93b1716.jpeg`} alt="Circle" className="rounded-full w-24 h-24" />
+        <img src={`https://raw.githubusercontent.com/56b81caaa87941618cfed6dfb4d34047/Testing_Platform_1732544491/${window.MI_PROJECT_GIT_REF || 'main'}/src/assets/images/02dfdbde981d4596aa48a513a93b1716.jpeg`} alt="Hero" className="w-24 h-24 mt-0" />
       </div>
-    </header>
+    </div>
   );
 };
 
-export { Header as component };
+export { Hero as component }
